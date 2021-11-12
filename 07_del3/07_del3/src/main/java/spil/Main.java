ca272a4e5b5fe0276f94c119e4f2bd7d2b46b666
Kasper <s205434@student.dtu.dk>
2021-11-12 10:39:34 +0100

    Added GUI_Field up to 24

--- 07_del3/07_del3/src/main/java/spil/Main.java
+++ 07_del3/07_del3/src/main/java/spil/Main.java
@@ -24,9 +24,22 @@ public class Main {
         GUI_Field guiField9 =new Field(Color.BLUE, Color.white,FieldConstants.THE_WEREWALL_ID, FieldConstants.THE_WEREWALL_AMOUNT, FieldConstants.THE_WEREWALL_NAME, FieldConstants.THE_WEREWALL_TEXT);
         GUI_Field guiField10 =new Field(Color.BLUE, Color.white,FieldConstants.THE_PIT_ID, FieldConstants.THE_PIT_AMOUNT, FieldConstants.THE_PIT_NAME, FieldConstants.THE_PIT_TEXT);
         GUI_Field guiField11 =new Field(Color.BLUE, Color.white,FieldConstants.GOLD_MINE_ID, FieldConstants.GOLD_MINE_AMOUNT, FieldConstants.GOLD_MINE_NAME, FieldConstants.GOLD_MINE_TEXT);
+        GUI_Field guiField12 =new Field(Color.BLUE, Color.white,FieldConstants.GOLD_MINE_ID, FieldConstants.GOLD_MINE_AMOUNT, FieldConstants.GOLD_MINE_NAME, FieldConstants.GOLD_MINE_TEXT);
+        GUI_Field guiField13 =new Field(Color.BLUE, Color.white,FieldConstants.GOLD_MINE_ID, FieldConstants.GOLD_MINE_AMOUNT, FieldConstants.GOLD_MINE_NAME, FieldConstants.GOLD_MINE_TEXT);
+        GUI_Field guiField14 =new Field(Color.BLUE, Color.white,FieldConstants.GOLD_MINE_ID, FieldConstants.GOLD_MINE_AMOUNT, FieldConstants.GOLD_MINE_NAME, FieldConstants.GOLD_MINE_TEXT);
+        GUI_Field guiField15 =new Field(Color.BLUE, Color.white,FieldConstants.GOLD_MINE_ID, FieldConstants.GOLD_MINE_AMOUNT, FieldConstants.GOLD_MINE_NAME, FieldConstants.GOLD_MINE_TEXT);
+        GUI_Field guiField16 =new Field(Color.BLUE, Color.white,FieldConstants.GOLD_MINE_ID, FieldConstants.GOLD_MINE_AMOUNT, FieldConstants.GOLD_MINE_NAME, FieldConstants.GOLD_MINE_TEXT);
+        GUI_Field guiField17 =new Field(Color.BLUE, Color.white,FieldConstants.GOLD_MINE_ID, FieldConstants.GOLD_MINE_AMOUNT, FieldConstants.GOLD_MINE_NAME, FieldConstants.GOLD_MINE_TEXT);
+        GUI_Field guiField18 =new Field(Color.BLUE, Color.white,FieldConstants.GOLD_MINE_ID, FieldConstants.GOLD_MINE_AMOUNT, FieldConstants.GOLD_MINE_NAME, FieldConstants.GOLD_MINE_TEXT);
+        GUI_Field guiField19 =new Field(Color.BLUE, Color.white,FieldConstants.GOLD_MINE_ID, FieldConstants.GOLD_MINE_AMOUNT, FieldConstants.GOLD_MINE_NAME, FieldConstants.GOLD_MINE_TEXT);
+        GUI_Field guiField20 =new Field(Color.BLUE, Color.white,FieldConstants.GOLD_MINE_ID, FieldConstants.GOLD_MINE_AMOUNT, FieldConstants.GOLD_MINE_NAME, FieldConstants.GOLD_MINE_TEXT);
+        GUI_Field guiField21 =new Field(Color.BLUE, Color.white,FieldConstants.GOLD_MINE_ID, FieldConstants.GOLD_MINE_AMOUNT, FieldConstants.GOLD_MINE_NAME, FieldConstants.GOLD_MINE_TEXT);
+        GUI_Field guiField22 =new Field(Color.BLUE, Color.white,FieldConstants.GOLD_MINE_ID, FieldConstants.GOLD_MINE_AMOUNT, FieldConstants.GOLD_MINE_NAME, FieldConstants.GOLD_MINE_TEXT);
+        GUI_Field guiField23 =new Field(Color.BLUE, Color.white,FieldConstants.GOLD_MINE_ID, FieldConstants.GOLD_MINE_AMOUNT, FieldConstants.GOLD_MINE_NAME, FieldConstants.GOLD_MINE_TEXT);
+        GUI_Field guiField24 =new Field(Color.BLUE, Color.white,FieldConstants.GOLD_MINE_ID, FieldConstants.GOLD_MINE_AMOUNT, FieldConstants.GOLD_MINE_NAME, FieldConstants.GOLD_MINE_TEXT);
 
         // We are making an Array list for GU Field
-        GUI_Field[] guiFields = new GUI_Field[11];
+        GUI_Field[] guiFields = new GUI_Field[24];
 
         guiFields[0] = guiField1;
         guiFields[1] = guiField2;
@@ -39,6 +52,19 @@ public class Main {
         guiFields[8] = guiField9;
         guiFields[9] = guiField10;
         guiFields[10] = guiField11;
+        guiFields[11] = guiField12;
+        guiFields[12] = guiField13;
+        guiFields[13] = guiField14;
+        guiFields[14] = guiField15;
+        guiFields[15] = guiField16;
+        guiFields[16] = guiField17;
+        guiFields[17] = guiField18;
+        guiFields[18] = guiField19;
+        guiFields[19] = guiField20;
+        guiFields[20] = guiField21;
+        guiFields[21] = guiField22;
+        guiFields[22] = guiField23;
+        guiFields[23] = guiField24;
 
 
         GUI gui = new GUI(guiFields);
